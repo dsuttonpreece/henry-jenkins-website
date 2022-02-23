5a7e3c532edc6cbd62b09e75658ac0d74bec73a2
dsuttonpreece <danielsuttonp@gmail.com>
2022-02-23 06:47:58 +0000

    Deploy https://github.com/dsuttonpreece/henry-jenkins-website/commit/f52925ac8c800ae79ef651796c5ae0e63d08d69b

--- assets/PreactCounter.54270b8f.js
+++
@@ -1,1 +0,0 @@
-import{l as e,e as t}from"./vendor.1c215f03.js";function d(){const[r,c]=e(0),o=()=>c(n=>n+1);return t("div",{id:"preact",class:"counter",children:[t("button",{onClick:()=>c(n=>n-1),children:"-"}),t("pre",{children:r}),t("button",{onClick:o,children:"+"})]})}export{d as default};
