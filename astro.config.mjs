1375c5a00de0424dd2d6f86605eb49fc64f6ceef
Daniel Sutton <danielsuttonp@gmail.com>
2022-03-02 19:55:59 +1100

    Update astro config with custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 export default {
-  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // pages: './src/pages', // Path to Astro components, pages, and data
-  // dist: './dist',       // When running `astro build`, path to final static output
-  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
-    site: "https://dsuttonpreece.github.io/henry-jenkins-website/",
+    site: "https://www.henryjenkins.com.au",
     sitemap: true, // Generate sitemap (set to "false" to disable)
   },
   devOptions: {
-    // hostname: 'localhost',  // The hostname to run the dev server on.
-    port: 3003, // The port to run the dev server on.
+    port: 3003,
   },
   renderers: ["@astrojs/renderer-preact"],
 };
